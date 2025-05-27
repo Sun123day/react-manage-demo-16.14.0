b392141d9de1106a2b48b91242dfc551e2bbad76
Sun123day <2478496130@qq.com>
2025-05-27 18:01:05 +0800

    feat:修改头部导航栏=》添加了退出的操作

--- src/layout/components/PaHeader/index.jsx
+++ src/layout/components/PaHeader/index.jsx
@@ -1,23 +1,65 @@
 import styles from './index.module.scss'
 
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Space, Dropdown } from 'antd';
+import { useNavigate } from "react-router-dom";
+
+import { 
+  Space, 
+  Dropdown,
+  Button, 
+  Modal,
+} from 'antd';
 import {
   HomeOutlined,
   QuestionCircleOutlined
 } from '@ant-design/icons';
 const PaHeader = () => {
+  
+  const navigate = useNavigate();
+
   const items = [
     {
-      key: '1',
+      key: 'account',
       label: '账户设置',
     },
     {
-      key: '2',
+      key: 'logout',
       label: '退出登录',
     },
   ];
+  const onClick = ({ key }) => {
+    console.log('click ', key); 
+    switch (key) {
+      case 'account':
+        console.log('账户设置');
+        break;
+      case 'logout':
+        console.log('退出登录');
+        LogOut()
+        break;
+      default:
+        break;
+    }
+  }
+
+  const LogOut = () => {
+    showModal()
+  }
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+    navigate('/login'); 
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  
 
   return (
     <div className={styles.headerBox}>
@@ -34,15 +76,31 @@ const PaHeader = () => {
           <HomeOutlined/>
           <QuestionCircleOutlined style={{margin: '0 16px', }} />
 
-          <Dropdown menu={{ items }}>
-            <a style={{marginRight: '16px', }}>
+          <Dropdown menu={{ items, onClick }}>
+            <a 
+              onClick={(e) => e.preventDefault()}
+              style={{marginRight: '16px', }}
+            >
               这是昵称
             </a>
           </Dropdown>
         </Space>
         
       </div>
+      
+      <>
+        <Modal
+          title="确认提示"
+          closable={{ 'aria-label': 'Custom Close Button' }}
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <p>确定要退出登录吗？</p>
+        </Modal>
+      </>
     </div>
+    
   )
 }
 export default PaHeader
