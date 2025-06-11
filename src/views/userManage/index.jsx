4e9945338430fe1a7722f3cdc65b1cf50dcd7288
Sun123day <2478496130@qq.com>
2025-06-11 09:38:43 +0800

    docs:进行了一些注释=》侧边栏部分取消了会导致头部导航栏的悬浮框隐藏的样式，这部分仍在解决中

--- src/views/userManage/index.jsx
+++ src/views/userManage/index.jsx
@@ -4,7 +4,15 @@ import React from 'react'
 
 const UserManage = () => {
   return (
-    <div>这个是用户管理</div>
+    <div>这个是用户管理
+      <div>
+        接下来要在这里完成的内容有：
+        1，表格的封装（含分页控制）
+        2，弹窗的封装，使用new this.$pageModal(FormModal, {}, (result) => {})的这种方式
+        3，看看能不能把删除的封装了，不然太难受了，每次警告框架需要配置太多内容
+    
+      </div>
+    </div>
   )
 }
 export default UserManage
